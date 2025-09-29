14ff0dc9918816e581b966777e602991cadf9570
vandan-savla <vdsavla@gmail.com>
2025-09-29 23:24:00 +0530

    minor changes

--- Day - 2/sg.tf
+++ Day - 2/sg.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "apache2_web_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "apache2_web_sg_ingress_ssh" {
   security_group_id = aws_security_group.apache2_web_sg.id
-  cidr_ipv4         = "103.190.190.237/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 resource "aws_vpc_security_group_ingress_rule" "apache2_web_sg_ingress_http" {
   security_group_id = aws_security_group.apache2_web_sg.id
-  cidr_ipv4         = "103.190.190.237/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
